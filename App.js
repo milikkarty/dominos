b4c8f377e9af67871d82e16d053bad12daa95bbd
milikkarty <milikkarty@gmail.com>
2022-08-02 13:30:04 +0300

    Add basic structure to the app

--- App.js
+++ App.js
@@ -1,20 +1,123 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView, Image, Platform } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+
+      <ScrollView style={styles.main}>
+
+        <ScrollView style={styles.menu} horizontal>
+          <View style={styles.menuItem}>
+            <Image />
+          </View>
+          <View style={styles.menuItem}>
+            <Image />
+          </View>
+          <View style={styles.menuItem}>
+            <Image />
+          </View>
+          <View style={styles.menuItem}>
+            <Image />
+          </View>
+        </ScrollView>
+
+        <ScrollView style={styles.news} horizontal>
+          <View style={styles.newsCard}>
+            <Image />
+            <Text>-30%</Text>
+          </View>
+          <View style={styles.newsCard}>
+            <Image />
+            <Text>-30%</Text>
+          </View>
+          <View style={styles.newsCard}>
+            <Image />
+            <Text>-30%</Text>
+          </View>
+          <View style={styles.newsCard}>
+            <Image />
+            <Text>-30%</Text>
+          </View>
+          <View style={styles.newsCard}>
+            <Image />
+            <Text>-30%</Text>
+          </View>
+        </ScrollView>
+
+        <Text style={styles.heading}>Check out these pizzas!</Text>
+
+        <View style={styles.pizza}>
+          <View style={styles.pizzaCard}>
+            <Image />
+            <Text>Pizza</Text>
+          </View>
+          <View style={styles.pizzaCard}>
+            <Image />
+            <Text>Pizza</Text>
+          </View>
+          <View style={styles.pizzaCard}>
+            <Image />
+            <Text>Pizza</Text>
+          </View>
+          <View style={styles.pizzaCard}>
+            <Image />
+            <Text>Pizza</Text>
+          </View>
+          <View style={styles.pizzaCard}>
+            <Image />
+            <Text>Pizza</Text>
+          </View>
+          <View style={styles.pizzaCard}>
+            <Image />
+            <Text>Pizza</Text>
+          </View>
+        </View>
+
+      </ScrollView>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'yellow',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? 50 : 0
   },
+  main: {},
+  menu: {
+    marginBottom: 20,
+    marginHorizontal: 5
+  },
+  menuItem: {
+    backgroundColor: 'blue',
+    height: 100,
+    marginHorizontal: 20,
+    width: 55
+  },
+  news: {
+    paddingBottom: 10,
+    marginHorizontal: 20
+  },
+  newsCard: {
+    backgroundColor: 'red',
+    height: 100,
+    marginHorizontal: 5,
+    width: 300
+  },
+  heading: {
+    fontSize: 20,
+    marginLeft: 25
+  },
+  pizza: {
+    alignItems: 'center'
+  },
+  pizzaCard: {
+    backgroundColor: 'green',
+    height: 200,
+    marginVertical: 20,
+    width: '80%'
+  }
 });
